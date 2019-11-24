7e5e8af6e251e423b3b54d84b9342548e75bcf86
bens4 <58140412+bens4@users.noreply.github.com>
2019-11-24 17:42:45 +0200

    Add files via upload

---
+++ Banking System.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Bank
+{
+    class Customer
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string gender { get; set; }
+        public string account_num { get; set; }
+        public string account_type { get; set; }
+        public string currency { get; set; }
+        public double i_balance { get; set; }
+        public double balance { get; set; }
+
+        public Customer()
+        {
+            Console.WriteLine("First Name: ");
+            this.first_name = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            this.last_name = Console.ReadLine();
+            Console.WriteLine("Gender: Male|Female");
+            this.gender = Console.ReadLine();
+            Console.WriteLine("Account Number: ");
+            this.account_num = Console.ReadLine();
+            Console.WriteLine("Account Type: ");
+            this.account_type = Console.ReadLine();
+            Console.WriteLine("Choose Account Currency: frw|US dollar|EURO");
+            this.currency = Console.ReadLine();
+            
+
+            if (currency == "FRW")
+            {
+                this.i_balance = 50000;
+            }
+            else if ((currency == "US dollar")|| (currency == "EURO"))
+            {
+                this.i_balance = 50;
+            }
+            this.balance = 0;
+        }
+
+        public Customer(string first_name, string last_name, string gender, string account_num, string account_type, string currency, double i_balance, double balance)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.gender = gender;
+            this.account_num = account_num;
+            this.account_type = account_type;
+            this.currency = currency;
+            this.i_balance = i_balance;
+            this.balance = balance;
+        }
+
+        public string displayInfo()
+        {
+            return string.Format(
+                "First Name: {0} \nLast Name: {1} \nGender: {2} \nAccount Number: {3} \nAccount Type: {4} \nAccoount Currency: {5} \nInitial Balance: {6}",
+                first_name, last_name, gender, account_num, account_type, currency, i_balance);
+        }
+
+        public double depositOperation(double depositAmount)
+        {
+            balance = i_balance + depositAmount;
+            return balance;
+        }
+
+        public double withdrawOperation(double withdrawAmount)
+        {
+            balance = i_balance - withdrawAmount;
+            return balance;
+        }
+        
+        static void Main(string[] args)
+        {
+            Customer cust01 = new Customer();
+            Console.WriteLine("Review Information: Y|N ");
+            string check = Console.ReadLine();
+            if (check == "Y")
+            {
+                Console.WriteLine(cust01.displayInfo());
+            }
+            else
+            {
+                Console.WriteLine("OKAY");
+            }
+            Console.WriteLine("Perform Operation: 1->Deposit|2->Withdraw");
+            string op = Console.ReadLine();
+            if (op == "1")
+            {
+                Console.WriteLine("Amount to Deposit: ");
+                double amt = Convert.ToDouble(Console.ReadLine());
+                cust01.depositOperation(amt);
+            }
+            else if (op == "2")
+            {
+                Console.WriteLine("Amount to Withdraw: ");
+                double amt = Convert.ToDouble(Console.ReadLine());
+                cust01.withdrawOperation(amt);
+            }
+
+        }
+    }
+}
---
+++ Salary Calculator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Salary
+{
+    class Employee
+    {
+        public string employe_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public char gender { get; set; }
+        public string position { get; set; }
+        public string employe_category { get; set; }
+        public double gross_salary { get; set; }
+        public double N_salary { get; set; }
+
+        public Employee()
+        {
+            Console.WriteLine("Employee ID: ");
+            this.employe_id = Console.ReadLine();
+            Console.WriteLine("First Name: ");
+            this.first_name = Console.ReadLine();
+            Console.WriteLine("Last Name: ");
+            this.last_name = Console.ReadLine();
+            Console.WriteLine("Gender: ");
+            this.gender = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Position: ");
+            this.position = Console.ReadLine();
+            Console.WriteLine("Employee Category: Monthly|Hourly|One-Time");
+            this.employe_category = Console.ReadLine();
+            Console.WriteLine("Employee Gross Salary: ");
+            this.gross_salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public Employee(string employe_id, string first_name, string last_name, char gender, string position, string employe_category,
+            double gross_salary, double N_salary)
+        {
+            this.employe_id = employe_id;
+            this.first_name = first_name;
+            this.gender = gender;
+            this.position = position;
+            this.employe_category = employe_category;
+            this.gross_salary = gross_salary;
+            this.N_salary = N_salary;
+        }
+
+        public string empInfo()
+        {
+            return string.Format(
+                "Employere ID: {0} \nFirst Name: {1} \nLast Name: {2} \nGender: {3} \nEmployee Position: {4} \nEmployee Category: {5} \nEmployee Gross Salary: {6} \n",
+                employe_id, first_name, last_name, gender, position, employe_category, gross_salary);
+        }
+
+        public double netSalary()
+        {
+            double tax, rssb, insurance, rate;
+            if (employe_category == "Monthly")
+            {
+                tax = (30 * gross_salary) / 100;
+                rssb = (5 * gross_salary) / 100;
+                insurance = (3 * gross_salary) / 100;
+                N_salary = gross_salary- (tax + rssb + insurance);
+            }
+            else if (employe_category == "Hourly")
+            {
+                double hrs;
+                Console.WriteLine("Number of Hours Worked: ");
+                hrs= Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Rate Per Hour: ");
+                rate = Convert.ToDouble(Console.ReadLine());
+                rssb = (5 * gross_salary) / 100;
+                insurance = (3 * gross_salary) / 100;
+                N_salary = (hrs * rate) - (rssb + insurance);
+            }
+            else if (employe_category == "One-Time")
+            {
+                tax = (30 * gross_salary) / 100;
+                N_salary = gross_salary - tax;
+            }
+            return N_salary;
+        }
+        static void Main(string[] args)
+        {
+            Employee emp01 = new Employee();
+            Console.WriteLine("Review Information: Y|N ");
+            string check = Console.ReadLine();
+            if (check == "Y")
+            {
+                Console.WriteLine(emp01.empInfo());
+            }
+            else
+            {
+                Console.WriteLine("OKAY");
+            }
+            Console.WriteLine("Calculate Net Salary: Y|N ");
+            check = Console.ReadLine();
+            if (check == "Y")
+            {
+                Console.WriteLine("Net Salary: "+emp01.netSalary());
+            }
+            else
+            {
+                Console.WriteLine("That's All");
+            }
+        }
+    }
+}
